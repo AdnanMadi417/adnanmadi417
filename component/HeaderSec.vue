a9c0c736b560e3e60e3c7d9898c0e58316a88ae6
AdnanMadi417 <164775717+AdnanMadi417@users.noreply.github.com>
2024-11-20 07:20:51 +0800

    I deployment my project using vercel app

--- component/HeaderSec.vue
+++ component/HeaderSec.vue
@@ -25,7 +25,7 @@ onMounted(() => {
 
       <div class="container-box">
         <div class="logo">
-          <a href="/">
+          <a href="/https://adnanmadi-417.vercel.app/">
             <span>Adnan Madi</span>
           </a>
           <button @click="toggleLinksVisibility" class="bar-btn" v-if="isMobile">

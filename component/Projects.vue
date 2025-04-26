b03000effbc300778113b047b23b61030da49d15
adnanmadi417 <adnanmadi417@gmail.com>
2025-04-26 18:40:54 +0800

    changed projects design and updated pics

--- component/Projects.vue
+++ component/Projects.vue
@@ -1,28 +1,49 @@
 <script setup lang="ts">
-import {computed, ref} from "vue";
 
 const projects = [
   {
+    title: "Masjid Albukhary KL Website",
+    description: "Full Stack Developer",
+    location: "Commerce Dot Com",
+    image: "./images/Masjid_Albukhary_KL_Website.png",
+    descriptions: [
+      "Built the official website for Masjid Albukhary KL from scratch, focusing on responsive design, digital engagement, and community service.",
+      "Integrated dynamic prayer times, multi-language support (English & Malay), and video gallery with YouTube integration.",
+      "Implemented admin dashboard using Django for announcements, event management, and content updates with secure authentication.",
+      "Developed venue rental request system with filtering, PDF generation by booking status (Pending, In Progress, Completed, Rejected), and improved communication flow.",
+    ],
+    links: [
+      {
+        buttonText: "Live Demo",
+        buttonLink: "http://www.masjidalbukharykl.com/",
+      }
+    ]
+  },
+  {
     title: "AIU Hostel Management System",
-    description: "Final Year Project",
+    description: "Frontend Developer",
+    location: "Albukhary International University",
+    image: "./images/AIU_Hostel_Management_System.png",
     descriptions: [
       "Developed and implemented a Hostel Management System, streamlining room allocation, maintenance requests, and billing, leading to a 50% reduction in manual administrative tasks.",
       "Optimized user interfaces for staff and residents, resulting in a 30% increase in user satisfaction and a 25% faster response time for issue resolution.",
     ],
     links: [
       {
-        buttonText: "Learn more",
+        buttonText: "Live Demo",
         buttonLink: "https://aiu-fyp.github.io/fyp-frontend2/home/",
       },
-      {
-        buttonText: "Github Link ",
-        buttonLink: "https://example.com/education-journey",
-      }
+      // {
+      //   buttonText: "Github Link",
+      //   buttonLink: "https://example.com/education-journey",
+      // }
     ]
   },
   {
     title: "IESCO Scholarship Application",
-    description: "Freelancer",
+    description: "Frontend Developer",
+    location: "The International Educational Scientific and Cultural Organization",
+    image: "./images/IESCO_Scholarship_Application.png",
     descriptions: [
       "Developed a Scholarship Registration System that streamlined application processes, reducing submission time by 40% and decreasing application processing by 30%.",
       "Designed and implemented user-friendly interfaces, resulting in a 25% increase in successful applicant registrations and improved the overall user experience for submitting documents.",
@@ -32,226 +53,268 @@ const projects = [
         buttonText: "Live Demo",
         buttonLink: "https://application.iesco.my",
       },
-      {
-        buttonText: "Github Link ",
-        buttonLink: "https://github.com/AdnanMadi417/iesco-frontend",
-      }
+      // {
+      //   buttonText: "Github Link",
+      //   buttonLink: "https://github.com/AdnanMadi417/iesco-frontend",
+      // }
     ]
   },
   {
     title: "MyChild Website",
-    description: "Front End Developer",
+    description: "Frontend Developer",
+    image: "./images/MyChild_Website.png",
+    location: "Side Project",
     descriptions: [
-      "Streamlined content management by enabling publishing of articles, sharing of stories, and showcasing organizational activities, improving visibility and engagement. Implemented multi-language support (Malay & English)",
-      "Developed a seamless donation system with Stripe integration and manual payment options, increasing fundraising efficiency"
+      "Streamlined content management by enabling publishing of articles, sharing of stories, and showcasing organizational activities, improving visibility and engagement. Implemented multi-language support (Malay & English).",
+      "Developed a seamless donation system with Stripe integration and manual payment options, increasing fundraising efficiency."
     ],
     links: [
       {
         buttonText: "Live Demo",
         buttonLink: "https://sideprojectngo.github.io/FrontEnd/home",
       },
-      {
-        buttonText: "Github Link ",
-        buttonLink: "https://github.com/AdnanMadi417/FrontEnd.git",
-      }
+      // {
+      //   buttonText: "Github Link",
+      //   buttonLink: "https://github.com/AdnanMadi417/FrontEnd.git",
+      // }
     ]
   },
 ]
-
-const currentIndex = ref(0);
-const itemsPerPage = 3;
-
-const visibleProjects = computed(() =>
-    projects.slice(currentIndex.value, currentIndex.value + itemsPerPage)
-);
-
-function nextPage() {
-  currentIndex.value = (currentIndex.value + itemsPerPage) % projects.length;
-}
-
-function prevPage() {
-  currentIndex.value = (currentIndex.value - itemsPerPage + projects.length) % projects.length;
-}
-
 </script>
 
 <template>
-  <div class="projects-sec" id="projects">
-    <h1>Relevant Projects</h1>
-    <div class="container">
-      <div class="card" v-for="project in visibleProjects" :key="project.title">
-        <div class="card-header">
-        <span>
+  <section class="project-section">
+    <h2>Relevant Projects</h2>
+    <div class="project-grid" v-for="(project, index) in projects" :key="project.title">
+      <template v-if="index % 2 === 0">
+        <div class="text-content">
+          <h3 class="project-title">{{ project.title }}</h3>
+          <p class="project-location">
             <UIcon
-                name="tabler-augmented-reality-2"
+                name="mdi-users"
+                class="icon"
             />
-          </span>
-          <div class="box">
-            <h2> {{ project.title }}</h2>
-            <h2>{{ project.description }}</h2>
+            {{ project.location }}
+             | {{ project.description }}
+          </p>
+          <ul class="project-description">
+            <li v-for="(desc, i) in project.descriptions" :key="i">{{ desc }}</li>
+          </ul>
+          <div class="project-links">
+            <a v-for="(link, i) in project.links" :key="i" :href="link.buttonLink" target="_blank" class="project-link">
+              {{ link.buttonText }}
+            </a>
           </div>
         </div>
-        <div class="card-content">
-          <ul>
-            <li v-for="(description, index) in project.descriptions" :key="index">{{ description }}</li>
-          </ul>
+        <div class="image-content">
+          <img
+              :src="project.image"
+              :alt="project.title"
+              class="image"
+              loading="lazy"
+          />
         </div>
-        <div class="card-footer">
-          <a v-for="(link, linkIndex) in project.links" :key="linkIndex" :href="link.buttonLink" class="card-button"
-             target="_blank">
-            {{ link.buttonText }}
-          </a>
+      </template>
+      <template v-else>
+        <div class="image-content">
+          <img
+              :src="project.image"
+              :alt="project.title"
+              class="image"
+              loading="lazy"
+          />
         </div>
-      </div>
-    </div>
-    <div class="buttons">
-      <button @click="prevPage">
-        <UIcon
-            name="uil-arrow"
-        />
-      </button>
-      <button @click="nextPage">
-        <UIcon
-            name="uil-arrow"
-        />
-      </button>
-    </div>
+        <div class="text-content">
+          <h3 class="project-title">{{ project.title }}</h3>
+          <p class="project-location">
 
-  </div>
+            <UIcon
+                name="mdi-users"
+                class="icon"
+            />
+
+            {{ project.location }}
+             | {{ project.description }}
+
+          </p>
+          <ul class="project-description">
+            <li v-for="(desc, i) in project.descriptions" :key="i">{{ desc }}</li>
+          </ul>
+          <div class="project-links">
+            <a v-for="(link, i) in project.links" :key="i" :href="link.buttonLink" target="_blank" class="project-link">
+              {{ link.buttonText }}
+            </a>
+          </div>
+        </div>
+      </template>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-.projects-sec {
-  width: 100%;
-  padding: 5rem 1rem;
-  background-color: var(--main-font-color);
-}
-
-.projects-sec h1 {
-  text-align: center;
-  font-size: 2rem;
-  text-transform: capitalize;
-  font-weight: bold;
-  margin-bottom: 4rem;
-  color: var(--main-color);
-}
-
-
-.container {
-  display: flex;
-  flex-wrap: wrap;
+section {
+  margin: 5rem auto;
   max-width: 1200px;
-  margin: 0 auto;
-  gap: 2rem 0;
-}
-
-.card {
-  flex: 1 1 350px;
+  padding: 0 1rem;
   display: block;
-  max-width: 370px;
-  background-color: var(--font-hovor-color);
-  padding: 2rem 1rem;
-  box-shadow: rgba(0, 0, 0, 0.2) 0 2px 8px 0;
+  height: 100%;
 }
 
+section h2 {
+  font-size: 2rem;
+  color: var(--main-color);
+  margin-bottom: 2rem;
+  text-align: center;
+}
 
-.card-header {
-  display: flex;
-  justify-content: space-around;
+.project-grid {
+  width: 100%;
+  margin: 2rem auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+}
+
+.project-title {
+  text-align: start;
+  font-size: 1.8rem;
+  color: var(--main-hovor-color);
+  font-weight: bold;
+  border-bottom: 2px solid var(--main-hovor-color);
+  padding-bottom: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+.project-role {
   font-size: 1.2rem;
   color: var(--main-color);
+  margin-bottom: 1rem;
+  font-style: italic;
 }
 
-.box {
-  align-items: center;
-  font-size: 1.2rem;
-}
-
-.card-header span {
-  font-size: 3rem;
-}
-
-@media (max-width: 1200px ) {
-  .card-header {
-    font-size: 1rem;
-    padding: 0;
-    margin: 0;
+.project-location {
+  font-size: 1.1rem;
+  color: var(--main-color);
+  margin-bottom: 1rem;
+  .icon {
+    color: var(--main-color);
+    margin-right: 1rem;
   }
-
-  .card-header span {
-    font-size: 2rem;
-  }
-
 }
 
-.card-content {
-  height: 280px;
-}
-
-.card-content ul li {
+.project-description {
   text-align: justify;
-  font-size: 1rem;
-  padding: 1rem 0.5rem 0;
+  font-size: 1.1rem;
+  line-height: 1.6;
+  color: var(--main-hovor-color);
+  padding-left: 1.2rem;
+  margin-bottom: 1.5rem;
 }
 
-.card-footer {
+.project-description li {
+  margin-bottom: 0.5rem;
+}
+
+.project-links {
   display: flex;
-  justify-content: space-around;
+  gap: 1rem;
   margin-top: 1rem;
 }
 
-.card-footer a {
-  padding: .5rem 1rem;
+.project-link {
+  display: inline-block;
+  padding: 0.5rem 1.2rem;
   background-color: var(--main-color);
-  border-radius: 1rem;
-  color: var(--main-font-color);
+  color: white;
+  text-decoration: none;
+  border-radius: 4px;
+  font-weight: 500;
+  transition: background-color 0.3s ease;
 }
 
-.card-footer a:hover {
-  background-image: linear-gradient(-20deg, var(--main-hovor-color), var(--main-color));
-  transition: background-image 0.3s ease-in-out;
+.project-link:hover {
+  background-color: var(--main-hovor-color);
 }
 
-@media (min-width: 768px) {
-  .container {
-    justify-content: space-between;
-  }
-}
-
-@media (max-width: 1200px ) {
-
-  .card-header {
-    font-size: 1rem;
-    padding: 0;
-    margin: 0;
-  }
-
-  .card-header span {
-    font-size: 2rem;
-  }
-
-}
-
-
-.buttons {
+.image-content {
+  max-width: 100%;
   display: flex;
   justify-content: center;
-  margin: 1rem 0 3rem 0;
+  align-items: center;
 }
 
-.buttons button {
-  padding: 0 1rem;
-  margin: 0 2rem;
-  font-size: 2rem;
-  background-color: var(--main-color);
-  color: var(--font-hovor-color);
-  border-radius: 0 1rem;
-  cursor: pointer;
+.image-content img {
+  width: 100%;
+  max-width: 500px;
+  height: auto;
+  aspect-ratio: 16/9;
+  object-fit: cover;
+  transition: transform 0.3s ease-in-out;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
-button:hover {
-  background-color: var(--main-hovor-color);
-  color: var(--font-hovor-color);
-  transition: .3s ease-in-out;
+.image-content img:hover {
+  transform: scale(1.05);
+}
+
+@media (max-width: 1024px) {
+  .project-grid {
+    gap: 2rem;
+    width: 100%;
+  }
+
+  .image-content img {
+    max-width: 100%;
+  }
+}
+
+@media (max-width: 768px) {
+  .project-grid {
+    grid-template-columns: 1fr;
+    gap: 2rem;
+    width: 100%;
+  }
+
+  .project-section {
+    margin: 3rem auto;
+  }
+
+  .image-content img {
+    max-width: 100%;
+  }
+
+  .project-title {
+    font-size: 1.5rem;
+  }
+}
+
+@media (max-width: 480px) {
+  .project-section {
+    margin: 2rem auto;
+    padding: 0 0.5rem;
+  }
+
+  .image-content img {
+    max-width: 100%;
+  }
+
+  .project-description {
+    font-size: 1rem;
+  }
+
+  .project-title {
+    font-size: 1.3rem;
+  }
+
+  .project-links {
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .project-link {
+    width: 100%;
+    text-align: center;
+  }
 }
 </style>
